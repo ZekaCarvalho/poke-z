6390bae8f1c98012c2de861bbd26c04ce9b33caa
ZekaCarvalho <smart.artdigital@gmail.com>
2020-07-08 22:48:28 -0300

    Adição da pasta build

---
+++ build/precache-manifest.01b3e1a14cb66d6ac83c4338f19c56e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08859d7900d7416ac37ed800f4de26fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef5587eb4dfd1484fbc4",
+    "url": "/static/css/main.ac93efb3.chunk.css"
+  },
+  {
+    "revision": "916df44d6af23386be96",
+    "url": "/static/js/2.917383a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.917383a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5587eb4dfd1484fbc4",
+    "url": "/static/js/main.bdf38088.chunk.js"
+  },
+  {
+    "revision": "993964759eea07e97f16",
+    "url": "/static/js/runtime-main.a811b6fc.js"
+  },
+  {
+    "revision": "b903bba66a650e99d257e75932f0fcf5",
+    "url": "/static/media/background.b903bba6.png"
+  }
+]);
